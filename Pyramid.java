01c272c4f698396afb91894a079e7dff5b30a7e3
ARAVINDH.K <19ee004@kpriet.ac.in>
2021-12-23 09:23:27 +0530

    Create Pyramid.java

---
+++ Pyramid.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+class triangle{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(n%2==0)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(((i+j)%2!=0 && i+j>=n-1))
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+                for(int j=1;j<n;j++)
+                {
+                    if((i+j)%2==0 && i>=j)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+            }
+            else
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(((i+j)%2==0 && i+j>=n-1))
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+                for(int j=1;j<n;j++)
+                {
+                    if((i+j)%2==0 && i>=j)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+
+ 
