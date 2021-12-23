63cd7a4f4ccaf89649728e28e8ee064162a00798
ARAVINDH.K <19ee004@kpriet.ac.in>
2021-12-23 09:22:09 +0530

    Create Fish.java

---
+++ Fish.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+class fish{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i>=j)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            for(int j=1;j<n;j++){
+                if(i+j>=n-1)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            for(int j=1;j<n;j++){
+                if(i>=j)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+        System.out.print("\n");
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i+j<=n-1)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            for(int j=1;j<n;j++){
+                if(i<=j)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            for(int j=1;j<n;j++){
+                if(i+j<=n-1)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+        System.out.print("\n");
+        }
+    sc.close();
+    }
+}
+
+ 
